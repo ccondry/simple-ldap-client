8e5371b589258cc3c98608ae75a7d2e5e73ed362
Coty Condry <ccondry@cisco.com>
2017-08-08 22:58:45 -0500

    remove unused files

--- ldapClient.js
+++
@@ -1,7 +0,0 @@
-const ldap = require('ldapjs')
-const client = ldap.createClient({ url: process.env.ldap_url, reconnect: true })
-client.on('error', (err) => {
-    console.warn('LDAP connection terminated on far-end, but it should reconnect.', err);
-})
-// our ldap client
-module.exports = client
