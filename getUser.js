94a57a4da154c5d25ba27cb485d5ad05914cf8b9
Coty Condry <ccondry@cisco.com>
2020-01-08 09:14:48 -0600

    catch connection errors in getUser

--- getUser.js
+++ getUser.js
@@ -12,31 +12,38 @@ function getUser ({ upn, password, attributes }) {
       reject('upn, and password are required')
     }
     const client = this.getClient()
-    // login to LDAP
-    client.bind(upn, password, async (err) => {
-      // console.log('ldap client bind')
-      if (err) {
-        console.log(err)
-        client.destroy(err)
-        reject(err)
-      }
-      // figure out which filter to use to identify the user
-      const filter = '(userPrincipalName=' + upn + ')'
-      // find user by filter
-      const opts = {
-        filter,
-        scope: 'sub',
-        attributes
-      }
+    // catch LDAP connection errors
+    client.on('connectError', function (err) {
+      console.log('Error connecting to LDAP:', err)
+    })
+    // wait for LDAP to connect
+    client.on('setup', function (clt, cb) {
+      // login to LDAP
+      client.bind(upn, password, async (err) => {
+        // console.log('ldap client bind')
+        if (err) {
+          console.log(err)
+          client.destroy(err)
+          reject(err)
+        }
+        // figure out which filter to use to identify the user
+        const filter = '(userPrincipalName=' + upn + ')'
+        // find user by filter
+        const opts = {
+          filter,
+          scope: 'sub',
+          attributes
+        }
 
-      try {
-        const user = await utils.getUser.call(this, client, this.baseDn, opts)
-        client.destroy()
-        resolve(user)
-      } catch (e) {
-        client.destroy()
-        reject(e)
-      }
+        try {
+          const user = await utils.getUser.call(this, client, this.baseDn, opts)
+          client.destroy()
+          resolve(user)
+        } catch (e) {
+          client.destroy()
+          reject(e)
+        }
+      })
     })
   })
 }
